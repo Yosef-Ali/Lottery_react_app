9146d33e9c2e02b051b3c9067d31a90b65eabace
Yosef-Ali <yosethio@yahoo.com>
2020-05-30 14:49:44 +0300

    frist lottory app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import Lottery from './Lottery'
+
+function App() {
+	return (
+		<div className="App">
+			<Lottery />
+		</div>
+	)
+}
+
+export default App
